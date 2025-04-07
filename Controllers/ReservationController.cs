4246f8b73747469630a6fad9172d64881149d1f3
l-lavapie071 <l.lavapie071@mybvc.ca>
2025-04-06 18:59:32 -0600

    Updates for reservation list]

---
+++ Controllers/ReservationController.cs
@@ -0,0 +1,32 @@
+﻿using Vehicle_Rental_Management_System.Data;
+using Vehicle_Rental_Management_System.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Vehicle_Rental_Management_System.Controllers
+{
+
+    [Authorize]
+    [Route("[controller]")]
+    public class ReservationController : Controller
+    {
+        private readonly App_Dbcontext _context;
+
+        public ReservationController(App_Dbcontext context)
+        {
+            _context = context;
+        }
+
+        // Display all Reservation
+        [HttpGet]
+        public async Task<IActionResult> ReservationList()
+        {
+            var reservations = await _context.Reservations
+            .Include(r => r.Customer)
+            .Include(r => r.Vehicle)
+            .ToListAsync();
+            return View(reservations);
+        }
+    }
+}
